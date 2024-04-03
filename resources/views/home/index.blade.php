f74e3e465bb94b40b4002fd8e993514e6d99346c
Johan Alfonso <jdaalfonso@gmail.com>
2024-04-03 12:32:40 -0500

    Seleccion vista de home

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,120 +3,19 @@
 <link rel="stylesheet" href="{{asset('css/dashboard/style.css')}}">
     <br>
     <div class="contenedor-toast" id="contenedor-toast">
-    {{--  Aqui trae las notificaciones por medio de javaescript  --}}
-    {{--  <div class="toast exito" id="1">
-        <div class="contenido">
-            <div class="icono">
-                <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 16 16">
-                <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/>
-                </svg>
-            </div>
-            <div class="texto">
-                <p class="titulo">Exito!</p>
-                <p class="descripcion">Se ha agregado el tercero.</p>
-            </div>
-        </div>
-        <button class="btn-cerrar">
-            <div class="icono">
-                <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 16 16">
-                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-                </svg>
-            </div>
-        </button>
-    </div>  --}}
-    {{--  <div class="toast error" id="2">
-        <div class="contenido">
-            <div class="icono">
-                <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 16 16">
-                    <path d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-                </svg>
-            </div>
-            <div class="texto">
-                <p class="titulo">Atencion!</p>
-                <p class="descripcion">Se ha inactivado la persona</p>
-            </div>
-        </div>
-        <button class="btn-cerrar">
-            <div class="icono">
-                <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 16 16">
-                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-                </svg>
-            </div>
-        </button>
-    </div>  --}}
     </div>
     <!DOCTYPE html>
     <html lang="en">
-    
+
     <head>
-<<<<<<< HEAD
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Home</title>
-        <link rel="stylesheet" href="css/accesos_directos.css" />
-        <link rel="stylesheet" href="{{ asset('css/estilosbarra.css') }}" type="text/css">
-        <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet"/>
-        <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
-        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    </head>
-    <body id="body">
-        <header>
-            <div class="icon__menu">
-                <i class='bx bx-menu'id="btn_open"> </i>
-            </div>
-
-            <div class="log-modify">
-                <i class='bx bx-store-alt' ></i>
-            </div>
-
-            <div class="titulo">
-                <h1>Ferreteria la excelencia</h1>
-            </div>
-
-            <button id="settings-button">
-                <i class="bx bx-cog"></i>
-            </button>
-
-            <ul id="settings-menu" style="display: none;">
-                <li><a href="#">Cerrar sesión</a></li>
-                <li><a href="#">Salir</a></li>
-            </ul>
-            <div class="cerrar_sesion">
-                <a href="/logout">
-                    Cerrar sesion
-                    <i class="fa-solid fa-door-open"></i>
-                </a>
-                <p>Texto publico</p>
-                
-            </div>
-        </header>
-
-        <div class="menu__side" id="menu_side">
-                <div class="name__page">
-                    <i><img class="logos" src="./img/Logo.png" alt="" /></i>
-=======
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
     <body>
-    
+
         <div class="container">
-            
-    
+
+
             <!-- Main Content -->
             <main>
                 <h1>Panel Estadisticas</h1>
@@ -218,10 +117,9 @@
                             </div>
                         </div>
                     </div>
->>>>>>> Brayan
                 </div>
                 <!-- End of Analyses -->
-    
+
                 <!-- New Users Section -->
                 {{-- <div class="new-users">
                     <h2>Nuevos Usuarios</h2>
@@ -249,7 +147,7 @@
                     </div>
                 </div> --}}
                 <!-- End of New Users Section -->
-    
+
                 <!-- Recent Orders Table -->
                 <div class="recent-orders">
                     <h2>Recent Orders</h2>
@@ -291,10 +189,10 @@
                     <a href="#">Show All</a>
                 </div>
                 <!-- End of Recent Orders -->
-    
+
             </main>
             <!-- End of Main Content -->
-    
+
             <!-- Right Section -->
             <div class="right-section">
                 {{-- <div class="nav">
@@ -311,7 +209,7 @@
                             dark_mode
                         </span>
                     </div>
-    
+
                     <div class="profile">
                         <div class="info">
                             <p>Hey, <b>Reza</b></p>
@@ -321,7 +219,7 @@
                             <img src="images/profile-1.jpg">
                         </div>
                     </div>
-    
+
                 </div>
                 <!-- End of Nav -->
      --}}
@@ -332,7 +230,7 @@
                         <p>Fullstack Web Developer</p>
                     </div>
                 </div>
-    
+
                 <div class="reminders">
                     <div class="header">
                         <h2>Reminders</h2>
@@ -340,7 +238,7 @@
                             notifications_none
                         </span>
                     </div>
-    
+
                     <div class="notification">
                         <div class="icon">
                             <span class="material-icons-sharp">
@@ -359,7 +257,7 @@
                             </span>
                         </div>
                     </div>
-    
+
                     <div class="notification deactive">
                         <div class="icon">
                             <span class="material-icons-sharp">
@@ -378,7 +276,7 @@
                             </span>
                         </div>
                     </div>
-    
+
                     <div class="notification add-reminder">
                         <div>
                             <span class="material-icons-sharp">
@@ -387,7 +285,7 @@
                             <h3>Add Reminder</h3>
                         </div>
                     </div>
-    
+
                 </div>
             </div>
         </div>
